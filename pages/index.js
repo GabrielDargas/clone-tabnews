09633c0a46e40e0f28a2180f94c40aa29e45003a
Gabriel Dargas <68109894+GabrielDargas@users.noreply.github.com>
2023-11-14 08:24:28 -0300

    Modificação frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Usando GitHub Pages para publicação, espero que te ajude a pensar em outras formas de fazer</h1>
+    return <h1>Usando Vercel para publicação, espero que te ajude a pensar em outras formas de fazer</h1>
 }
 
 export default Home;
