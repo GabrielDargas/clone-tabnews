2d90bed24d23c378f30012a2360e251106489c41
Gabriel Dargas <68109894+GabrielDargas@users.noreply.github.com>
2023-12-03 14:23:24 -0300

    add `jsconfig.json` file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,7 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as SUMe;");
+  const result = await database.query("SELECT 1 + 1 as SUM;");
   console.log(result.rows);
   res.status(200).json({
     status: "Ok",
