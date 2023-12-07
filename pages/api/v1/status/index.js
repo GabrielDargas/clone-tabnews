bb410357992b3fe20d970ad264b8d8bb9662446f
Gabriel Dargas <68109894+GabrielDargas@users.noreply.github.com>
2023-12-07 19:04:43 -0300

    add new properties to `status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,26 @@
 import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as SUM;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+  const postgresVersion = parseFloat(
+    (await database.query("SHOW server_version;")).rows[0].server_version,
+  );
+  const maxConnections = parseInt(
+    (await database.query("SHOW max_connections;")).rows[0].max_connections,
+  );
+  const openedConnections = parseInt(
+    (
+      await database.query(
+        "SELECT COUNT(*) AS opened_connections FROM pg_stat_activity",
+      )
+    ).rows[0].opened_connections,
+  );
+
   res.status(200).json({
-    status: "Ok",
+    updated_at: updatedAt,
+    postgres_version: postgresVersion,
+    max_connections: maxConnections,
+    opened_connections: openedConnections,
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,19 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parseUpdatedAt);
+
+  expect(responseBody.postgres_version).toEqual(16);
+
+  expect(responseBody.max_connections).toEqual(100);
+
+  expect(responseBody.opened_connections).toBeGreaterThan(1);
+  expect(responseBody.opened_connections).toBeLessThan(
+    responseBody.max_connections,
+  );
 });
